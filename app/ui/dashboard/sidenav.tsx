69969d52d6cab80e133de82d1e12c8b80a4a233d
Dicky <muhammad.dickynn@gmail.com>
2023-11-14 20:57:11 +0700

    Fix sign out functionality and login button rendering

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -22,7 +22,12 @@ export default function SideNav() {
           'use server';
           await signOut();
         }}></form>
-        <form>
+        <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+        >
           <button className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3">
             <PowerIcon className="w-6" />
             <div className="hidden md:block">Sign Out</div>
